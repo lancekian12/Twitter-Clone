4cd87cd393d169766c3a1af315c672df82c396b0
Lance Kian F. Flores <lafa.flores.up@phinmaed.com>
2024-11-03 00:56:54 +0800

    use the data in the controller to show it in the view

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -9,6 +9,21 @@ use Illuminate\Http\Request;
 class DashboardController extends Controller
 {
     public function index() {
-        return view('welcome');
+        $users = [
+            [
+                'name' => 'Alex',
+                'age' => 30,
+            ],
+            [
+                'name' => 'Dan',
+                'age' => 25,
+            ]
+            ,
+            [
+                'name' => 'John',
+                'age' => 17,
+            ]
+            ];
+        return view('dashboard', ['users' => $users]);
     }
 }
---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,11 @@
+
+
+@foreach ($users as $user)
+     <h1> {{ $user['name'] }}</h1>
+     <h2> {{ $user['age'] }}</h2>
+@if ($user['age'] < 18)
+    <p>user can't drive</p>
+@endif
+     <hr>
+
+@endforeach
